9ccf4c0124c4f0ee6c991393b2181c0bdf2806e2
Vanek <b.ivan72@mail.ru>
2019-04-09 15:51:48 +0300

    add CodeClimate and Eslint and Travis-ci

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const readlineSync = require('readline-sync');
 
 const welcome = () => {
-    console.log("Welcome to the Brain Games!");
-    const name = readlineSync.question('May I have your name? ');
-    console.log('Hello, ' + name + '!');
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
 };
 
 export default welcome;
